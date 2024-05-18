0008550d65012201aff1b6f1899163e79f63766d
Vadym <telcom.odessa@gmail.com>
2024-05-18 20:50:52 +0300

    initial

---
+++ Test 7_4.py
@@ -0,0 +1,21 @@
+
+import os
+import time
+
+#directory = '.' # Замените на путь к вашему каталогу
+#directory = 'D:/0-Мое обучение/UrbanUniversity/+7 Модуль/+7МодульТильда 9/python_snippets'
+directory = 'D:/+7 Модуль/+7МодульТильда 9/python_snippets'
+
+print()
+for root, dirs,files in os.walk(directory):
+    for file in files:
+        filepath = os.path.join(root, file)
+        filetime = os.path.getmtime(filepath)
+        formatted_time = time.strftime("%d.%m.%Y %H:%M", time.localtime(filetime))
+        filesize = os.path.getsize(filepath)
+        parent_dir = os.path.dirname(filepath)
+    #    print(f'Обнаружен файл: {file}, Путь: {filepath}, Размер: {filesize} байт, Время изменения: {formatted_time}, Родительская директория: {parent_dir}')
+        print(f'Обнаружен файл: {file};   Путь: {filepath};   Размер: {filesize} байт, ')
+        print(f'                Время изменения: {formatted_time};    Родительская директория: {parent_dir}')
+
+
